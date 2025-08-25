2093dca699611baca17a06bd4b2cee035f628325
Shivang Ayush <shivangayush161161@gmail.com>
2025-08-25 20:03:27 +0530

    adding speaking agent

---
+++ speaking agent.py
@@ -0,0 +1,12 @@
+# This is the sample of speech to text converter!
+# Take a look at this stuff!
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hey, Dude Welcome to the virtual den of the KNOWNUSERONE")
+engine.runAndWait()
+
+# For default speaking and LOW code!
+import pyttsx3
+pyttsx3.speak("I will speak this text")
+
--- test.py
+++ test.py
@@ -1,18 +1,5 @@
 print("Hello, Worldz1")
 
 import pyjokes
-joke1=pyjokes.get_joke()
-joke2=pyjokes.get_joke()
-joke3=pyjokes.get_joke()
-joke4=pyjokes.get_joke()
-joke5=pyjokes.get_joke()
-joke6=pyjokes.get_joke()
-joke7=pyjokes.get_joke()
-
-print(joke1)
-print(joke2)
-print(joke3)
-print(joke4)
-print(joke5)
-print(joke6)
-print(joke7)
+joke=pyjokes.get_joke()
+print(joke)
