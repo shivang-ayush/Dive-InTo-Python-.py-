bf3f4d461f977b34fcf666ddadfaf18b442156c7
Shivang Ayush <shivangayush161161@gmail.com>
2025-08-26 18:02:40 +0530

    Comment added!

--- speaking agent.py
+++ speaking agent.py
@@ -5,8 +5,10 @@ import pyttsx3
 engine = pyttsx3.init()
 engine.say("Hey, Dude Welcome to the virtual den of the KNOWNUSERONE")
 engine.runAndWait()
+print("Ran the code successfully!")
 
 # For default speaking and LOW code!
 import pyttsx3
 pyttsx3.speak("I will speak this text")
+print("Ran the code successfully!")
 
